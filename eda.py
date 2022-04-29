80412201586c96ad219171f79dc4cc6bc588a809
Jader Martins <jadermcs94@gmail.com>
2022-04-29 20:55:31 -0300

    nltk fix

--- eda.py
+++ eda.py
@@ -60,6 +60,7 @@ def get_only_chars(line):
 #for the first time you use wordnet
 import nltk
 nltk.download('wordnet')
+nltk.download('omw-1.4')
 from nltk.corpus import wordnet 
 
 def synonym_replacement(words, n):
