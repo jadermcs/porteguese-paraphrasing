40a6da2e9c77904feb0b3062a7e0bb50338670f4
Jader Martins <jadermcs94@gmail.com>
2022-05-11 20:48:00 -0300

    update

--- run.py
+++ run.py
@@ -7,15 +7,15 @@ from train.actor_critic_ppo import ppo_trainer
 
 def main():
     wandb.init(project="rl-paraphrasing")
-    # generate_data()
+    generate_data()
 
-    # critic_train([
-    #     "--num_train_epochs", "20",
-    # ])
+    critic_train([
+        "--num_train_epochs", "20",
+    ])
     
-    # actor_train([
-    #     "--num_train_epochs", "20",
-    # ])
+    actor_train([
+        "--num_train_epochs", "20",
+    ])
 
     ppo_trainer([
         "--num_train_epochs", "20",
